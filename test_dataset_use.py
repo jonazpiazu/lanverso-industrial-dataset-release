6e8a3b2dd5ad4098f42c953de947c2c58e255b89
Jon Azpiazu <jonazpiazu@users.noreply.github.com>
2023-11-29 15:08:32 +0100

    Linting file

--- test_dataset_use.py
+++ test_dataset_use.py
@@ -1,8 +1,11 @@
 """Minimal example of how to use the IndustrialDataset class to load and interact with the dataset."""
 
-from lanverso_industrial_dataset_helper.lanverso_industrial_dataset_helper import IndustrialDataset as industrial_dataset
+from lanverso_industrial_dataset_helper.lanverso_industrial_dataset_helper import (
+    IndustrialDataset as industrial_dataset,
+)
 import open3d as o3d
 import json
+
 
 def test_all_datasets():
     for scene in industrial_dataset.scene_list():
@@ -10,6 +13,7 @@ def test_all_datasets():
         for pcd_path in dataset.paths:
             pcd = o3d.io.read_point_cloud(pcd_path)
             print(pcd)
+
 
 if __name__ == "__main__":
     dataset = industrial_dataset()
@@ -21,7 +25,7 @@ if __name__ == "__main__":
     pcd = o3d.io.read_point_cloud(dataset.paths[0])
     o3d.visualization.draw_geometries([pcd])
 
-    dataset = industrial_dataset(scene='EvenTableTwoPartsZivid')
+    dataset = industrial_dataset(scene="EvenTableTwoPartsZivid")
     for pcd_path in dataset.paths:
         pcd = o3d.io.read_point_cloud(pcd_path)
         print(pcd)
